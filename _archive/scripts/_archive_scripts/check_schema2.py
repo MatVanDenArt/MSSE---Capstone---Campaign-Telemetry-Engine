1a7b3a3beb8186a5b4febfa4b3802ec966ecfdb8
MatVanDenArt <mpservice@hotmail.co.uk>
2026-09-06 21:36:52 +0100

    chore: untrack archives, scratchpads, and seed.sql from repository

--- _archive/scripts/_archive_scripts/check_schema2.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('capstone.db')
-c = conn.cursor()
-c.execute('SELECT sql FROM sqlite_master WHERE type="table" AND name="ga4_events"')
-print(c.fetchone()[0])
-c.execute('SELECT sql FROM sqlite_master WHERE type="table" AND name="mailchimp_events"')
-print(c.fetchone()[0])
--- _archive/scripts/_archive_scripts/list_tables.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-
-c = sqlite3.connect('capstone.db')
-print([row[0] for row in c.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()])
